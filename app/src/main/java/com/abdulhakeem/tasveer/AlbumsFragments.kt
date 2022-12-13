d72db5bd05f28c1276599eec6e8e529053349605
Abdul Hakeem <ahm.mhd78@gmail.com>
2022-12-13 11:10:54 +0500

    fragments added

---
+++ app/src/main/java/com/abdulhakeem/tasveer/AlbumsFragments.kt
@@ -0,0 +1,7 @@
+package com.abdulhakeem.tasveer
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AlbumsFragments : Fragment(R.layout.fragment_albums_fragments)
---
+++ app/src/main/java/com/abdulhakeem/tasveer/PhotosFragment.kt
@@ -0,0 +1,7 @@
+package com.abdulhakeem.tasveer
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PhotosFragment : Fragment(R.layout.fragment_album_photos)
