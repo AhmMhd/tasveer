0d301734e855755ce6a77ca23d879f7b63eb7650
Abdul Hakeem <ahm.mhd78@gmail.com>
2022-12-14 11:09:03 +0500

    ui tweaks

--- app/src/main/java/com/abdulhakeem/tasveer/DataBinding.kt
+++ app/src/main/java/com/abdulhakeem/tasveer/DataBinding.kt
@@ -4,6 +4,13 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+
 
 @BindingAdapter("bind:adapter")
 fun RecyclerView.setAdapter(adapter: BaseAdapter<*, *>) {
@@ -11,4 +18,8 @@ fun RecyclerView.setAdapter(adapter: BaseAdapter<*, *>) {
 }
 
 @BindingAdapter("bind:load")
-fun ImageView.load(path: String) = Glide.with(context).load(path).into(this)
+fun ImageView.load(path: String) {
+    Glide.with(context).load(path)
+        .transform(MultiTransformation(CenterCrop(), RoundedCorners(12)))
+        .sizeMultiplier(0.5f).into(this)
+}
--- app/src/main/java/com/abdulhakeem/tasveer/Extensions.kt
+++ app/src/main/java/com/abdulhakeem/tasveer/Extensions.kt
@@ -3,21 +3,16 @@ package com.abdulhakeem.tasveer
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.os.Build
+import android.os.Environment
+import android.util.TypedValue
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
-fun Context.hasStoragePermission() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-    ContextCompat.checkSelfPermission(
-        this,
-        Manifest.permission.READ_MEDIA_IMAGES
-    ) == PackageManager.PERMISSION_GRANTED
-            &&
-            ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_MEDIA_VIDEO
-            ) == PackageManager.PERMISSION_GRANTED
+fun Context.hasStoragePermission() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+    Environment.isExternalStorageManager()
 } else {
     ContextCompat.checkSelfPermission(
         this,
@@ -37,6 +32,7 @@ fun Fragment.requestStoragePermission(callback: (Boolean) -> Unit) {
                 val isGranted = it.value
                 hasAllPermissionsGranted = isGranted
             }
+
             callback.invoke(hasAllPermissionsGranted)
         }
 
@@ -48,7 +44,18 @@ fun Fragment.requestStoragePermission(callback: (Boolean) -> Unit) {
             )
         )
     } else {
-        requestPermissionLauncher.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+        requestPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        )
     }
 
 }
+
+val Number.toPx
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        this.toFloat(),
+        Resources.getSystem().displayMetrics
+    )
