123ad967cc1c6fb09631c21a547784af7de5d20d
Abdul Hakeem <ahm.mhd78@gmail.com>
2022-12-15 09:52:54 +0500

    thumbnail added

--- app/src/main/java/com/abdulhakeem/tasveer/ui/common/DataBinding.kt
+++ app/src/main/java/com/abdulhakeem/tasveer/ui/common/DataBinding.kt
@@ -2,6 +2,7 @@ package com.abdulhakeem.tasveer.ui.common
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.abdulhakeem.tasveer.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -10,6 +11,7 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 @BindingAdapter("bind:load")
 fun ImageView.load(path: String) {
     Glide.with(context).load(path)
+        .placeholder(R.drawable.thumbnail)
         .transform(MultiTransformation(CenterCrop(), RoundedCorners(12)))
         .sizeMultiplier(0.5f).into(this)
 }
